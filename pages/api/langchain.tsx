af88bfe9264c0e4aec2150d92e627a9bc53cfb4c
marcelborbus <29482604+marcelborbus@users.noreply.github.com>
2023-04-11 15:28:40 +0000

    new apikey

--- pages/api/langchain.tsx
+++ pages/api/langchain.tsx
@@ -1,9 +1,5 @@
 import { OpenAI } from "langchain/llms/openai";
 
-//const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-//const OPENAI_API_KEYs = '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';
-console.log(process.env.OPENAI_API_KEY)
-
 const model = new OpenAI({openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0});
 
 export default model; 
